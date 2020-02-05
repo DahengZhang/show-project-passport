8d1315a229ac1e941632c2c23caad175fe1c759e
DahengZhang <Daheng_Zhang@outlook.com>
2020-02-06 02:03:39 +0800

    线上链条最终版本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
 const { getArgv } = require('./config/util')
 
+let args = []
+getArgv('secret') && args.push(`--secret=${getArgv('secret')}`)
+getArgv('password') && args.push(`--password=${getArgv('password')}`)
+getArgv('account') && args.push(`--account=${getArgv('account')}`)
+getArgv('database') && args.push(`--database=${getArgv('database')}`)
+getArgv('collection') && args.push(`--collection=${getArgv('collection')}`)
+
 module.exports = {
     apps: [{
         name: 'show-project-passport',
-        script: `npm run start -- --secret=${getArgv('secret')} --password=${getArgv('password')} --account=${getArgv('account')} --database=${getArgv('database')} --collection=${getArgv('collection')}`,
+        script: `npm run start -- ${args.join(' ')}`,
         instances: 1,
         autorestart: true,
         watch: false,
